9c45af0753bcf4a6594487f3ec88e55730cc7827
Lartg <roc.gerrety@gmail.com>
2021-10-14 13:32:04 -0800

    Styled more

--- recipes.js
+++ recipes.js
@@ -18,7 +18,7 @@ const frenchPress =
   <figure><img src="IMAGES/french press.jpeg"></figure>
     <ol>
       <li>Heat around 1 quart of water to 205 degrees</li>
-      <li>Grind 50g of coffee course</li>
+      <li>Grind 60g of coffee course</li>
       <li>Add ground coffee to the bottom of your french press</li>
       <li>Fill your french press quickly to saturate all your coffee, and start a timer</li>
       <li>Pop the lid back on the french press to insulate, but do not press!</li>
@@ -30,15 +30,24 @@ const frenchPress =
 
 const coldBrew = 
 `<div>
-  <h2>Cold Brew</h2>
-  <figure><img src="IMAGES/coldbrew.jpeg"><figure>
+  <h2>Cold Brew:</h2>
+  <figure><img src="IMAGES/coldbrew.jpeg"></figure>
+  <h3>Tools:<h3>
+  <ol>
+    <li>Large spoon</li>
+    <li>Large pot or foodsafe bucket (~2 gallons)</li>
+    <li>Kitchen sieve fine enough to catch grounds</li>
+    <li>Pitcher for storage</li>
+  </ol>
+  <h3>Recipe:</h3>
   <ol>
     <li>Time: 12-16 hours, recommend brewing overnight</li>
     <li>Get your pound of coffee ground as course as possible where you bought it</li>
-    <li>Put your coffee in a large pot or foodsafe bucket, and combine with a gallon of water</li>
+    <li>Put coffee into the brewing vessel, and combine with a gallon of water</li>
     <li>Stir, put a lid on, and let sit for 12-16 hours</li>
     <li>after 12 hours, taste once per hour until finished</li>
     <li>Record that time for repeatability</li>
+    <li>Filter through sieve into a pitcher</li>
     <li>Refridgerate, and dilute with water when consuming</li>
     <li>Cold Brew is very strong, I recommend starting at 1 part Cold Brew : 2 Parts water</li>
   </ol>
@@ -46,7 +55,7 @@ const coldBrew =
 
 const pourOver = 
 `<div>
-  <h2>Pour Over</h2>
+  <h2>Pour Over:</h2>
   <figure><img src="IMAGES/Basically-Pour-Over-lede.jpeg"></figure>
   <h3>Tools:</h3>
   <ol>
@@ -56,7 +65,7 @@ const pourOver =
     <li>Home burr grinder</li>
     <li>Filtered funnel</li>
   </ol>
-  <h3>Recipe</h3>
+  <h3>Recipe:</h3>
   <p>
     There isn't a recipe I can recommend for a perfect pour-over coffee.
     With so many variables in equipment, you just have to taste a lot of coffee, and know what you are looking for out of your brew.
